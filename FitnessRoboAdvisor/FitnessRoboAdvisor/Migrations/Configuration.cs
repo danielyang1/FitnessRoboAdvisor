f4ef5ec38849ee12639cedba0c625c0d813536c2
danielyang1 <dan0yang@outlook.com>
2016-03-23 12:26:07 -0500

    Initial

---
+++ FitnessRoboAdvisor/FitnessRoboAdvisor/Migrations/Configuration.cs
@@ -0,0 +1,46 @@
+namespace FitnessRoboAdvisor.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FitnessRoboAdvisor.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(FitnessRoboAdvisor.Models.ApplicationDbContext context)
+        {
+            if (!context.Users.Any(u => u.UserName == "admin@icloud.com"))
+            {
+                RoleStore<IdentityRole> roleStore = new RoleStore<IdentityRole>(context);
+                RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(roleStore);
+                UserStore<ApplicationUser> userStore = new UserStore<ApplicationUser>(context);
+                UserManager<ApplicationUser> userManager = new ApplicationUserManager(userStore);
+                ApplicationUser admin = new ApplicationUser { UserName = "admin@icloud.com" };
+
+                userManager.Create(admin, password: "password00");
+                roleManager.Create(new IdentityRole { Name = "admin" });
+                userManager.AddToRole(admin.Id, "admin");
+            }
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
